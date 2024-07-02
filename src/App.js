bc5a2b382f2d99d0fec3f8e26e6de327262a9050
Allison Artemis Zou <allison.artemis.zou@gmail.com>
2024-07-02 17:39:21 -0400

    Add numpad keys

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react';
+
+function NumDisplay({num}) {
+  return (
+    <div>
+      {num}
+    </div>
+  );
+}
+
+
+function NumPadButton({bigNum, setBigNum, num}) {
+  function handleClick() {
+    console.log('clicked');
+    if (num == bigNum) {
+      console.log('clicked bignum');
+      setBigNum(generateBigNum);
+    }
+  }
+  return (
+    <button onClick={handleClick}>{num}</button>
+  );
+}
+
+function NumPad(props) {
+  function handleKeyEvent (event) {
+    if (event.key == props.bigNum) {
+      props.setBigNum(generateBigNum);
+    }
+  }
+
+  return (
+    <div className='num-pad'>
+      <NumPadButton {...props} num='1'/>
+      <NumPadButton {...props} num='2'/>
+      <NumPadButton {...props} num='3'/>
+      <NumPadButton {...props} num='4'/>
+      <NumPadButton {...props} num='5'/>
+      <NumPadButton {...props} num='6'/>
+      <NumPadButton {...props} num='7'/>
+      <NumPadButton {...props} num='8'/>
+      <NumPadButton {...props} num='9'/>
+      <div></div>
+      <NumPadButton num='0'/>
+      <div></div>
+    </div>
+  );
+}
+
+function generateBigNum() {
+  return Math.floor(Math.random() * 10);
+}
 
 function App() {
+  const [bigNum, setBigNum] = useState(generateBigNum());
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <NumDisplay num={bigNum}/>
       </header>
+      <NumPad setBigNum={setBigNum} bigNum={bigNum}/>
     </div>
   );
 }
